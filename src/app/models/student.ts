ff3408520263882cdaadc13ae50cd5793bbf4473
Miguel Arciniega <milearciniegara@ittepic.edu.mx>
2020-11-09 19:45:54 -0700

    V1.0.1

---
+++ src/app/models/student.ts
@@ -0,0 +1,7 @@
+export class Student {
+
+    name: string;
+    controlnumer: string;
+    active: boolean;
+
+}
---
+++ src/app/services/student.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
+import { Student } from '../models/student';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private students: Student[] = new Array();
+
+  constructor() {
+    this.students.push({
+      name: 'Miguel Leopoldo Arciniega Ramirez',
+      controlnumer: '16400881',
+      active: true
+    });
+    this.students.push({
+      name: 'Blanca Estefani Ramirez Barajas',
+      controlnumer: '16400985',
+      active: true
+    });
+    this.students.push({
+      name: 'Edwin Antonio Arellano Mata',
+      controlnumer: '16400882',
+      active: false
+    });
+  }
+
+  getStudents(): Student[]{
+    return this.students;
+  }
+
+  changeStatus(position): void{
+    this.students[position].active = this.students[position].active;
+  }
+}
+
+// Para aplicar MVC
+// Hacer primero el modelo -> ionic g class models/student --type=model
+// Desupes el controlador -> ionic g service services/student
+// Por ultimo la vista -> archivo home.page.ts
